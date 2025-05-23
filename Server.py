10c0d0006278f6bb80c58c2949b8aa3dc5eedd7b
Furry NightShade <skyrelmchaos004@gmail.com>
2025-05-23 19:53:35 +0200

    Loading
    # TODO

--- Server.py
+++ Server.py
@@ -6,7 +6,8 @@ class Data:
     url1 = "https://voda.portabo.org/api/hlasice.json"
     url2 = "https://voda.portabo.org/api/aktualne.json"
     
-    def get_json(url: str):
+    @staticmethod
+    def _get_json(url: str):
         # Fetch the JSON data from the given URL
         # url: str - The URL to fetch the JSON data from
         # return: dict - The JSON data as a dictionary
@@ -18,13 +19,26 @@ class Data:
             raise Exception(f"Failed to fetch data from {url}")
 
     @staticmethod
+    def _clean_data(df: pd.DataFrame):
+        # Clean the DataFrame by removing unnecessary columns
+        # df: pd.DataFrame - The DataFrame to clean
+        # return: pd.DataFrame - The cleaned DataFrame
+        
+        # Drop the 'id' column from the DataFrame
+        # TODO
+        df = df
+        
+        # Return the cleaned DataFrame
+        return df
+    
+    @staticmethod
     def load():
         # Load the data from the URLs into pandas DataFrames
         # return: tuple - A dataframe containing the joined data from both URLs
         
         # Load the json files as json objects
-        json1 = Data.get_json(Data.url1)["hlasice"]
-        json2 = Data.get_json(Data.url2)["hlasice"]
+        json1 = Data._get_json(Data.url1)["hlasice"]
+        json2 = Data._get_json(Data.url2)["hlasice"]
         
         # Convert the json objects to pandas DataFrames
         df1 = pd.json_normalize(json1)
@@ -33,9 +47,12 @@ class Data:
         # Merge the two DataFrames on the 'id' column
         df2 = df2.merge(df1, left_on='ORP', right_on='ORP', how='left')
         
+        # Clean data by removing unnecessary columns
+        df2 = Data._clean_data(df2)
+        
         # Return the loaded DataFrames
         return df2
       
 data = Data.load()
-print(data)
-print(data.columns)
+# Save the DataFrame to a CSV file
+data.to_csv('data.csv', index=False)
